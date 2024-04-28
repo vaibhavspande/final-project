a8bb4ff3701b67ae1ac4920d6083de068a8b2833
Vaibhav Pande <vaibhavpande194@gmail.com>
2024-04-29 00:52:06 +0530

    env added and vercel.json

--- identeefi-backend-node/index.js
+++ identeefi-backend-node/index.js
@@ -1,43 +1,44 @@
 const express = require('express');
-const cors = require('cors'); 
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const { Client } = require('pg')
 const path = require('path');
-const multer = require('multer'); 
+const multer = require('multer');
+require('dotenv').config();
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-const port = 5000;
+const server = process.env.SERVER;
 
 const client = new Client({
-    host: "localhost",
-    user: "postgres",
-    port: 5432,
-    password: "Omsai@123",
-    database: "project1"
+    host: process.env.HOST,
+    user: process.env.USER,
+    port: process.env.PORT,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE
 })
 
 client.connect()
 
 // auth  
 
-function createAccount(username , password, role){
-    const res =  client.query('INSERT INTO auth (username, password, role) VALUES ($1, $2, $3)', [username, password, role], (err, res)=>{
-        if(err){
+function createAccount(username, password, role) {
+    const res = client.query('INSERT INTO auth (username, password, role) VALUES ($1, $2, $3)', [username, password, role], (err, res) => {
+        if (err) {
             console.log(err.message);
-        }else{
+        } else {
             console.log('Data insert successful');
         }
     })
 }
 
-function changePassword(username, password){
-    const res =  client.query('UPDATE auth SET password = $1 WHERE username = $2', [password, username], (err, res)=>{
-        if(err){
+function changePassword(username, password) {
+    const res = client.query('UPDATE auth SET password = $1 WHERE username = $2', [password, username], (err, res) => {
+        if (err) {
             console.log(err.message);
-        }else{
+        } else {
             console.log('Data update successful');
         }
     })
@@ -45,12 +46,12 @@ function changePassword(username, password){
 
 // profile
 
-function createProfile(username, name , description, website, location, image, role){
-    client.query('INSERT INTO profile (username, name, description, website, location, image, role) VALUES ($1, $2, $3, $4, $5, $6, $7)', 
-        [username, name, description, website, location, image, role], (err, res)=>{
-            if(err){
+function createProfile(username, name, description, website, location, image, role) {
+    client.query('INSERT INTO profile (username, name, description, website, location, image, role) VALUES ($1, $2, $3, $4, $5, $6, $7)',
+        [username, name, description, website, location, image, role], (err, res) => {
+            if (err) {
                 console.log(err.message);
-            }else{
+            } else {
                 console.log('Data insert successful');
             }
         })
@@ -73,12 +74,12 @@ const storageProfile = multer.diskStorage({
     }
 })
 
-function addProduct(serialNumber, name , brand){
-    client.query('INSERT INTO product (serialNumber, name, brand) VALUES ($1, $2, $3)', 
-        [serialNumber, name, brand], (err, res)=>{
-            if(err){
+function addProduct(serialNumber, name, brand) {
+    client.query('INSERT INTO product (serialNumber, name, brand) VALUES ($1, $2, $3)',
+        [serialNumber, name, brand], (err, res) => {
+            if (err) {
                 console.log(err.message);
-            }else{
+            } else {
                 console.log('Data insert successful');
             }
         })
@@ -87,52 +88,52 @@ function addProduct(serialNumber, name , brand){
 
 
 // auth
-app.get('/authAll', async (req, res)=>{
-    const data =  await client.query('Select * from auth');
+app.get('/authAll', async (req, res) => {
+    const data = await client.query('Select * from auth');
     res.header('Access-Control-Allow-Credentials', true);
     res.send(data.rows);
     console.log("Data sent successfully");
 });
 
-app.post('/auth/:username/:password', async (req, res)=>{
-    const {username, password} = req.params;
-    const data =  await client.query(`SELECT * FROM auth WHERE username = '${username}' AND password = '${password}'`);
+app.post('/auth/:username/:password', async (req, res) => {
+    const { username, password } = req.params;
+    const data = await client.query(`SELECT * FROM auth WHERE username = '${username}' AND password = '${password}'`);
     res.send(data.rows);
     console.log("Data sent successfully");
 });
 
-app.post('/addaccount', (req, res)=>{
-    const {username, password, role} = req.body;
+app.post('/addaccount', (req, res) => {
+    const { username, password, role } = req.body;
     createAccount(username, password, role);
     res.send('Data inserted');
 
 });
 
-app.post('/changepsw', (req, res)=>{
-    const {username, password} = req.body;
+app.post('/changepsw', (req, res) => {
+    const { username, password } = req.body;
     changePassword(username, password);
     res.send('Data updated');
 });
 
 // profile 
 
-app.get('/profileAll', async (req, res)=>{
-    const data =  await client.query('Select * from profile');
+app.get('/profileAll', async (req, res) => {
+    const data = await client.query('Select * from profile');
     res.header('Access-Control-Allow-Credentials', true);
     res.send(data.rows);
     console.log("Data sent successfully");
 });
 
-app.get('/profile/:username', async (req, res)=>{
-    const {username} = req.params;
-    const data =  await client.query(`SELECT * FROM profile WHERE username = '${username}'`);
+app.get('/profile/:username', async (req, res) => {
+    const { username } = req.params;
+    const data = await client.query(`SELECT * FROM profile WHERE username = '${username}'`);
     console.log(data);
     res.send(data.rows);
     console.log("Data sent successfully");
 });
 
-app.post('/addprofile', (req, res)=>{
-    const {username, name, description, website, location, image, role} = req.body;
+app.post('/addprofile', (req, res) => {
+    const { username, name, description, website, location, image, role } = req.body;
     createProfile(username, name, description, website, location, image, role);
     res.send('Data inserted');
 
@@ -140,14 +141,14 @@ app.post('/addprofile', (req, res)=>{
 
 // image
 
-app.post('/upload/profile', (req, res)=>{
+app.post('/upload/profile', (req, res) => {
 
-    let upload = multer({ storage: storageProfile}).single('image');
+    let upload = multer({ storage: storageProfile }).single('image');
 
-    upload(req, res, (err)=>{
-        if(!req.file){
+    upload(req, res, (err) => {
+        if (!req.file) {
             return res.send('Please select an image to upload')
-        }else if (err instanceof multer.MulterError){
+        } else if (err instanceof multer.MulterError) {
             return res.send(err);
         } else if (err) {
             return res.send(err);
@@ -157,14 +158,14 @@ app.post('/upload/profile', (req, res)=>{
 
 // product  
 
-app.post('/upload/product', (req, res)=>{
+app.post('/upload/product', (req, res) => {
 
-    let upload = multer({ storage: storageProduct}).single('image');
+    let upload = multer({ storage: storageProduct }).single('image');
 
-    upload(req, res, (err)=>{
-        if(!req.file){
+    upload(req, res, (err) => {
+        if (!req.file) {
             return res.send('Please select an image to upload')
-        }else if (err instanceof multer.MulterError){
+        } else if (err instanceof multer.MulterError) {
             return res.send(err);
         } else if (err) {
             return res.send(err);
@@ -173,30 +174,30 @@ app.post('/upload/product', (req, res)=>{
 })
 
 app.get('/file/profile/:fileName', function (req, res) {
-    const {fileName} = req.params;
+    const { fileName } = req.params;
     const filePath = path.join(__dirname, 'public/uploads/profile', fileName);
     res.sendFile(filePath);
 });
 
 app.get('/file/product/:fileName', function (req, res) {
-    const {fileName} = req.params;
+    const { fileName } = req.params;
     const filePath = path.join(__dirname, 'public/uploads/product', fileName);
     res.sendFile(filePath);
 });
 
 
-app.get('/product/serialNumber', async (req, res)=>{
-    const data =  await client.query(`SELECT serialNumber FROM product`);
+app.get('/product/serialNumber', async (req, res) => {
+    const data = await client.query(`SELECT serialNumber FROM product`);
     res.send(data.rows);
 });
 
-app.post('/addproduct', (req, res)=>{
-    const {serialNumber, name, brand} = req.body;
+app.post('/addproduct', (req, res) => {
+    const { serialNumber, name, brand } = req.body;
     addProduct(serialNumber, name, brand);
     res.send('Data inserted');
 
 });
 
-app.listen(port, ()=>{
+app.listen(server, () => {
     console.log('Server is running on port 5000');
 });
