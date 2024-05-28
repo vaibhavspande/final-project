2462fdab346261957d71b4283f73716288928e4f
Vaibhav Pande <91242365+vaibhavspande@users.noreply.github.com>
2024-05-28 11:03:36 +0530

    Update index.js

--- identeefi-backend-node/index.js
+++ identeefi-backend-node/index.js
@@ -5,16 +5,12 @@ const { Client } = require('pg')
 const path = require('path');
 const multer = require('multer');
 require('dotenv').config();
-const corsOptions = {
-  origin: 'https://final-project-ytai.vercel.app', // Your front-end URL
-  methods: ['GET', 'POST', 'PUT', 'DELETE'], // HTTP methods to be allowed
-  allowedHeaders: ['Content-Type', 'Authorization'], // Headers to be allowed
-};
+
 
 
 const app = express();
 app.use(bodyParser.json());
-app.use(cors(corsOptions));
+app.use(cors());
 
 const server = process.env.SERVER;
 
